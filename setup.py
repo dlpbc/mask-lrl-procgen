b1e694fe79e5969b0fed9a9444197c36f2de47c5
Ese <17943860+dlpbc@users.noreply.github.com>
2023-08-12 02:12:38 +0100

    added files to the issue3_debug branch

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='continual_rl',
+    version='1.0',
+    description='Continual reinforcement learning baselines and standard experiments.',
+    author='Sam Powers',
+    author_email='snpowers@cs.cmu.edu',
+    packages=find_packages(),
+    py_modules=['continual_rl.available_policies', 'continual_rl.experiment_specs'],
+    install_requires=['uuid',
+                      'numpy',
+                      'gym',
+                      'protobuf==3.20',
+                      'tensorboard',
+                      'torch-ac',
+                      'moviepy',
+                      'dotmap',
+                      'psutil',
+                      'opencv-python'
+                    ]
+)
